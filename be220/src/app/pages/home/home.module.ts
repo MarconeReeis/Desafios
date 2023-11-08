c58f8fa13fea99fc7c3d4338e1149121c75b3b65
Marcone Reis <marcone.trajano.reis@gmail.com>
2023-11-08 18:06:44 -0300

    Refatorando o header

--- be220/src/app/pages/home/home.module.ts
+++ be220/src/app/pages/home/home.module.ts
@@ -7,7 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
-
+import { HeaderComponent } from 'src/app/components/header/header.component';
 
 @NgModule({
   imports: [
@@ -16,7 +16,7 @@ import { HomePage } from './home.page';
     IonicModule,
     HomePageRoutingModule,
   ],
-  declarations: [HomePage],
+  declarations: [HomePage, HeaderComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomePageModule {}
