641dced8fa2dafcfcd96b2f69ff1fa423a6f79c8
Marcone Reis <marcone.trajano.reis@gmail.com>
2024-08-03 17:46:30 -0300

    feat: criando conexao e chamada no mysql

---
+++ sitcon/pacientes_backend/db.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = '127.0.0.1';
+$db = 'clinica';
+$user = 'root';
+$pass = '';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo 'Erro na conexão com o banco de dados: ' . $e->getMessage();
+    exit;
+}
